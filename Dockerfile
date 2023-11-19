62083f8a86751bf58db74e3a6b84a876b6a296d1
Felix Prattes <felix.prattes@gmail.com>
2023-11-19 17:14:57 +0100

    Fixed Version

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM ubuntu:latest
+LABEL authors="felix"
+
+ENTRYPOINT ["top", "-b"]
+
+# Install dependencies
+
+FROM node:latest
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install --production
+
+COPY . .
+
+ENV NODE_ENV=production
+ENV PORT=3000
+
+EXPOSE $PORT
+
+
+RUN npm run build
+CMD ["npm", "run"]
--- components/authform/authpage.tsx
+++ components/authform/authpage.tsx
@@ -1,9 +1,16 @@
 
+import Image from "next/image";
 
 export default function AuthenticationPage (){
     return(
-        <div className={"w-full min-w-screen min-h-screen h-full bg-red-400"}>
-            <h1 className={"bg-blue-500"}>Auth Page</h1>
+        <div className={"w-full flex flex-row min-w-screen min-h-screen h-full "}>
+            <div className={"w-full h-screen bg-blue-950"}>
+
+            </div>
+
+            <div className={"w-1/2 h-full flex items-center justify-center bg-white"}>
+                <h1 className={"text-black"}>Auth Page</h1>
+            </div>
         </div>
     )
 }
