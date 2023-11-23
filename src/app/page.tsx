ad013ab770d35cf0c0898bd3ae2626cd14007eba
Lexi4eg <felix.prattes@gmail.com>
2023-11-23 14:39:45 +0100

    New Auth Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,30 +3,29 @@ import Dashboard from "./(dashboards)/Dashboard";
 import AuthenticationPage from "../../components/authform/authpage";
 
 
-
 export default async function Home() {
-  const session = await getServerAuthSession();
-  const now = new Date();
-  let nightmode = "daymode";
+    const session = await getServerAuthSession();
+    const now = new Date();
+    let nightmode = "daymode";
     if(now.getHours() >= 18 || now.getHours() <= 6){
         nightmode = "nightmode";
     }
-  return (
-      <>
-          {session ? (
-            <>
-            <div className="flex bg-cover max-w-screen min-h-screen bg-center flex-col items-center justify-center text-white bg-gradient-to-b from-[#2e026d] to-[#15162c]" style={{ backgroundImage: nightmode === "nightmode" ? "none" : "url('/porsche.jpg')", backgroundColor: nightmode === "nightmode" ? "black" : "" }}>
+    return (
+        <>
+            {session ? (
+                <>
+                    <div className="flex bg-cover max-w-screen min-h-screen bg-center flex-col items-center justify-center text-white bg-gradient-to-b from-[#2e026d] to-[#15162c]" style={{ backgroundImage: nightmode === "nightmode" ? "none" : "url('/porsche.jpg')", backgroundColor: nightmode === "nightmode" ? "black" : "" }}>
 
-                 <Dashboard style = {nightmode} />
-            </div>
-            </>
+                        <Dashboard style = {nightmode} />
+                    </div>
+                </>
 
 
-        ) : (
+            ) : (
 
                 <AuthenticationPage />
 
-        )}
-      </>
-  );
-}
+            )}
+        </>
+    );
+};
