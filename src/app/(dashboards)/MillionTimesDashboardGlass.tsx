83ba21327d25a876a8034306a36ae78432762d12
Felix Prattes <felix.prattes@gmail.com>
2023-12-11 17:52:45 +0100

    New Navbar for the remote

---
+++ src/app/(dashboards)/MillionTimesDashboardGlass.tsx
@@ -0,0 +1,39 @@
+"use client";
+import React, { useEffect } from "react";
+
+interface Props {
+  style?: string;
+}
+
+export default function MillionTimesDashboardGlass(props: Props) {
+  useEffect(() => {
+    // @ts-ignore
+    //window.parent.document.body.style["zoom"] = "250%";
+    // Add global CSS rule to remove scrollbar
+    const style = document.createElement("style");
+    style.innerHTML = `
+      * {
+        overflow: hidden !important;
+      }
+    `;
+    document.head.appendChild(style);
+    return () => {
+      document.head.removeChild(style);
+    };
+  }, []);
+
+  return (
+    <>
+      <iframe
+        src="https://dra1ex.github.io/ClockClock/?theme=glass&bg=windows_light&clock_size=50&clock_margin=10&clock_width=5&clock_hour=18&clock_minute=22&fps=100"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      />
+    </>
+  );
+}
--- src/app/(dashboards)/Rootdashboard.tsx
+++ src/app/(dashboards)/Rootdashboard.tsx
@@ -5,20 +5,22 @@ import WorkClockDashboard from "~/app/(dashboards)/WorkClockDashboard";
 import MillionTimesDashboard from "~/app/(dashboards)/MillionTimesDashboard";
 import FlipDotClock from "~/app/(dashboards)/FlipDotClock/FlipDotClock";
 import SolarSystemWallpaper from "~/app/(dashboards)/solarSystem/solarSystemWallpaper";
-import ClockClock24FDashboard from "~/app/(dashboards)/ClockClock24F";
 
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
 
 const socket = io("http://192.168.178.57:3001"); // Replace with your server URL
 import { useRouter } from "next/navigation";
+import MillionTimesDashboardBlackWhite from "~/app/(dashboards)/MillionTimesDashboardBlackWhite";
+import MillionTimesDashboardGlass from "~/app/(dashboards)/MillionTimesDashboardGlass";
+import MillionTimesDashboardWood from "~/app/(dashboards)/MillionTimesDashboardWood";
 interface Props {
   style?: string;
   session: any;
 }
 
 export default function Rootdashboard({ style, session }: Props) {
-  const [selectedOption, setSelectedOption] = useState(1); // Set initial value to 1
+  const [selectedOption, setSelectedOption] = useState(7); // Set initial value to 1
   const router = useRouter();
   useEffect(() => {
     socket.on("mode", (newMode) => {
@@ -38,12 +40,17 @@ export default function Rootdashboard({ style, session }: Props) {
         {Number(selectedOption) === 3 && (
           <MillionTimesDashboard style={style} />
         )}
-        {Number(selectedOption) === 5 && <FlipDotClock style={style} />}
-        {Number(selectedOption) === 6 && <SolarSystemWallpaper style={style} />}
-        {Number(selectedOption) === 7 && (
-          <ClockClock24FDashboard style={style} />
+        {Number(selectedOption) === 4 && <MillionTimesDashboardBlackWhite />}
+        {Number(selectedOption) === 5 && (
+          <MillionTimesDashboardWood style={style} />
         )}
-        {![1, 2, 3, 5, 6, 7].includes(Number(selectedOption)) && (
+        {Number(selectedOption) === 6 && (
+          <MillionTimesDashboardGlass style={style} />
+        )}
+
+        {Number(selectedOption) === 7 && <SolarSystemWallpaper style={style} />}
+        {Number(selectedOption) === 8 && <FlipDotClock style={style} />}
+        {![1, 2, 3, 4, 5, 6, 7, 8].includes(Number(selectedOption)) && (
           <div>Unexpected mode: {selectedOption}</div>
         )}
       </div>
