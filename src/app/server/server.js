79f0d52c044c813d433185d8eef58ca6610dd445
Lexi4eg <felix.prattes@gmail.com>
2024-01-11 14:32:33 +0100

    New Web socket Infratructure

--- src/app/server/server.js
+++ src/app/server/server.js
@@ -1,13 +1,12 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-    // Handle HTTP requests if needed
+
 });
 
 const { Server } = require('socket.io');
 const io = new Server(server, {
     cors: {
         origin: ["http://192.168.178.57:3000","http://localhost:3000", "http://172.16.84.116:3000"   ],
-        // Allow only this origin
         methods: ["GET", "POST"]
     }
 });
@@ -18,6 +17,14 @@ io.on('connection', (socket) => {
     socket.on('mode', (mode) => {
         io.emit('mode', mode);
         console.log(mode)
+    });
+
+    //add tempeture
+
+    socket.on('temp', (temp) => {
+        io.emit('temp', temp);
+        console.log(temp)
+
     });
 
     socket.on('disconnect', () => {
