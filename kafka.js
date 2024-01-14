946d8b161492a5f46ed24aec01b8670e8619b878
Felix Prattes <felix.prattes@gmail.com>
2024-01-14 12:18:40 +0100

    Working Simple Kafka integration and working Docker image

--- kafka.js
+++ kafka.js
@@ -6,27 +6,17 @@ const kafka = new Kafka({
 });
 
 const producer = kafka.producer();
-const consumer = kafka.consumer({ groupId: "test-group" });
 
 const run = async () => {
   // Producing
   await producer.connect();
   await producer.send({
-    topic: "test-topic",
-    messages: [{ value: "Hello KafkaJS user!" }],
+    topic: "test",
+    messages: [{ value: "New message for test topic 2" }],
   });
 
-  // Consuming
-  await consumer.connect();
-  await consumer.subscribe({ topic: "test-topic", fromBeginning: true });
-
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      console.log({
-        value: message.value.toString(),
-      });
-    },
-  });
+  // Ensure the producer gracefully disconnects
+  await producer.disconnect();
 };
 
 run().catch(console.error);
---
+++ kafka2.js
@@ -0,0 +1,25 @@
+const { Kafka } = require("kafkajs");
+
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+});
+
+// Use a new group ID to consume all messages from the beginning
+const consumer = kafka.consumer({ groupId: "new-test-group" });
+
+const run = async () => {
+  // Consuming
+  await consumer.connect();
+  await consumer.subscribe({ topic: "test", fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        value: message.value.toString(),
+      });
+    },
+  });
+};
+
+run().catch(console.error);
