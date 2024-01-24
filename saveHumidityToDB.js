4cbcdb4a74bd163a1b7ebeefc8493904379ab185
Felix Prattes <felix.prattes@gmail.com>
2024-01-24 20:45:08 +0100

    Working Save to DB API

--- saveHumidityToDB.js
+++ saveHumidityToDB.js
@@ -1,26 +1,44 @@
-import prisma from "./prismaClient.js";
+import { Kafka } from "kafkajs";
+import { PrismaClient } from "@prisma/client";
 
-import { consumer } from "./kafkaConsumer.js";
+const prisma = new PrismaClient();
 
-const run = async () => {
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["localhost:9092"],
+});
+
+const consumer = kafka.consumer({ groupId: "mode" });
+
+const run = async (/** @type {undefined} */ options) => {
   // Consuming
   await consumer.connect();
   await consumer.subscribe({ topic: "temperatureData", fromBeginning: true });
+  await consumer.subscribe({ topic: "humidityData", fromBeginning: true });
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
-      // Parse the message value, assuming it's a JSON string
-      const data = JSON.parse(message.value.toString());
-
-      // Insert it into your PostgreSQL database
+      // Instead of logging the message value, insert it into your PostgreSQL database
       try {
-        const res = await prisma.temperature.create({
-          data: {
-            temperature: message.value.toString(),
-          },
-        });
-        console.log(res);
+        if (topic === "temperatureData") {
+          const value = parseFloat(message.value.toString());
+          const res = await prisma.temperature.create({
+            data: {
+              value: value,
+            },
+          });
+          console.log(res);
+        } else if (topic === "humidityData") {
+          const value = parseFloat(message.value.toString());
+          const res = await prisma.humidity.create({
+            data: {
+              value: value,
+            },
+          });
+          console.log(res);
+        }
       } catch (err) {
+        // @ts-ignore
         console.log(err.stack);
       }
     },
