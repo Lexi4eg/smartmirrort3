ac89359d8c7185380877eaf752dfc3a6354d1507
Felix Prattes <felix.prattes@gmail.com>
2024-06-15 18:53:47 +0200

    Production Version 0.74

--- components/Phone_Remote/functions/sendWidget.ts
+++ components/Phone_Remote/functions/sendWidget.ts
@@ -1,8 +1,8 @@
-import {Kafka} from "kafkajs";
+import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
   clientId: "my-app",
-  brokers: ["localhost:9092"],
+  brokers: ["broker:9092"],
 });
 
 const producer = kafka.producer();
--- src/app/api/sendHumidity/route.ts
+++ src/app/api/sendHumidity/route.ts
@@ -10,7 +10,7 @@ const producer: Producer = kafka.producer({
 });
 export async function POST(request: Request) {
   const data = await request.json();
-
+  console.log("send humidity data", data);
   const humidity: number = parseFloat(data.humidity);
 
   await producer.connect();
@@ -20,7 +20,6 @@ export async function POST(request: Request) {
     messages: [{ value: JSON.stringify(humidity) }],
   });
 
-  // Disconnect the producer
   await producer.disconnect();
   return new Response("OK", { status: 200 });
 }
