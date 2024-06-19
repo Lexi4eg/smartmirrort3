afdad611cedd96e0f1faedaaf0cf6cd7ceefd243
Felix Prattes <felix.prattes@gmail.com>
2024-06-19 20:26:14 +0200

    Production Version 0.76.2

--- components/WIdgets/NasaWidget.tsx
+++ components/WIdgets/NasaWidget.tsx
@@ -21,7 +21,7 @@ const NasaWidget = ({ style = "" }: NasaWidgetProps) => {
 
   const fetchNasaData = async () => {
     const response = await fetch(
-      "https://api.nasa.gov/planetary/apod?api_key=eQNlkWnRk6fiiYb51PXM9sB7IdTZZcUPhSKJrbjX",
+      "https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}",
     );
     const data: Props = await response.json();
     setNasaData(data);
